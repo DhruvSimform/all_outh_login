26374caa3a57a05a81b2bcb742aceea29082862d
Devarsh <devarsh.chhatrala@simformsolutions.com>
2025-07-07 18:27:48 +0530

    fix improvements

--- src/api/v1/auth_routes.py
+++ src/api/v1/auth_routes.py
@@ -1,9 +1,10 @@
 from decouple import config
 from fastapi import APIRouter, HTTPException, Request
 
+from src.main import logger
 from src.oauth.google import google
 from src.schemas.user_schema import TokenResponse
-from src.services.user_service import handle_oauth_user
+from src.services.user_service import handle_google_oauth_user
 
 router = APIRouter()
 
@@ -17,18 +18,17 @@ async def login_via_google(request: Request):
 @router.get("/callback/google", response_model=TokenResponse)
 async def google_callback(request: Request):
     token = await google.authorize_access_token(request)
-    print("OAuth token returned:", token)
 
     if "userinfo" not in token:
         raise HTTPException(status_code=400, detail="No userinfo in token response")
 
     try:
         user_info = token["userinfo"]
-        print("User info:", user_info)
+        logger.debug(f"User-info = {user_info}")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"User info parsing failed: {e}")
 
     # Store or fetch user from DB (based on oauth_accounts table now)
-    user, jwt_token = await handle_oauth_user(user_info, provider="google")
+    user, jwt_token = await handle_google_oauth_user(user_info, provider="google")
 
     return TokenResponse(access_token=jwt_token, user=user)  # Pydantic UserOut model
